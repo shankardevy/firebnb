df40d25ca851e23081592f179357cb6b5f93770c
Shankardevy <shankardevy@gmail.com>
2023-04-18 00:20:37 +0100

    Use slots for rendering title and price in room component

--- lib/firebnb_web/components/firebnb_components.ex
+++ lib/firebnb_web/components/firebnb_components.ex
@@ -16,4 +16,27 @@ defmodule FirebnbWeb.Components do
     </h1>
     """
   end
+
+  def room(assigns) do
+    ~H"""
+    <a href="">
+      <div>
+        <img
+          src="/images/rooms/1.webp"
+          class="rounded-lg shadow-lg object-none object-center w-80 h-80"
+        />
+        <h1 class="pt-3 font-bold text-gray-700 line-clamp-2">
+          <%= render_slot(@title) %>
+        </h1>
+      </div>
+
+      <div class="mt-2 text-sm text-gray-600">
+        Bangtok &bull; Thailand
+        <div class="font-bold text-gray-700">
+          €<%= render_slot(@price) %> <span class="font-normal">night</span>
+        </div>
+      </div>
+    </a>
+    """
+  end
 end
