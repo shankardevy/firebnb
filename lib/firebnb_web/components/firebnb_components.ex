0233d3031a68dcb9cc22aa1d99b560ccdea001d9
Shankardevy <shankardevy@gmail.com>
2023-04-18 00:47:38 +0100

    Complete room component with slots and attrs

--- lib/firebnb_web/components/firebnb_components.ex
+++ lib/firebnb_web/components/firebnb_components.ex
@@ -23,13 +23,19 @@ defmodule FirebnbWeb.Components do
 
   slot :title, required: true, doc: "Room title"
   slot :price, required: true, doc: "Room price"
+  slot :location, required: true, doc: "Room location"
+
+  slot :cover_image, required: true do
+    attr :src, :string, required: true
+  end
 
   def room(assigns) do
     ~H"""
     <a href="">
       <div>
         <img
-          src="/images/rooms/1.webp"
+          :for={image <- @cover_image}
+          src={image.src}
           class="rounded-lg shadow-lg object-none object-center w-80 h-80"
         />
         <h1 class="pt-3 font-bold text-gray-700 line-clamp-2">
@@ -38,7 +44,7 @@ defmodule FirebnbWeb.Components do
       </div>
 
       <div class="mt-2 text-sm text-gray-600">
-        Bangtok &bull; Thailand
+        <%= render_slot(@location) %>
         <div class="font-bold text-gray-700">
           €<%= render_slot(@price) %> <span class="font-normal">night</span>
         </div>
