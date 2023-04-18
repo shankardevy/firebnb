e5734eb155086d31b69ae24d80a6f834f2172d96
Shankardevy <shankardevy@gmail.com>
2023-04-18 08:54:14 +0100

    Add user authentication using phx.gen.auth

--- lib/firebnb_web/router.ex
+++ lib/firebnb_web/router.ex
@@ -1,6 +1,8 @@
 defmodule FirebnbWeb.Router do
   use FirebnbWeb, :router
 
+  import FirebnbWeb.UserAuth
+
   pipeline :browser do
     plug :accepts, ["html"]
     plug :fetch_session
@@ -8,16 +10,11 @@ defmodule FirebnbWeb.Router do
     plug :put_root_layout, {FirebnbWeb.Layouts, :root}
     plug :protect_from_forgery
     plug :put_secure_browser_headers
+    plug :fetch_current_user
   end
 
   pipeline :api do
     plug :accepts, ["json"]
-  end
-
-  scope "/", FirebnbWeb do
-    pipe_through :browser
-
-    live "/", HomeLive.Index
   end
 
   # Other scopes may use custom stacks.
@@ -39,6 +36,45 @@ defmodule FirebnbWeb.Router do
 
       live_dashboard "/dashboard", metrics: FirebnbWeb.Telemetry
       forward "/mailbox", Plug.Swoosh.MailboxPreview
+    end
+  end
+
+  ## Authentication routes
+
+  scope "/", FirebnbWeb do
+    pipe_through [:browser, :redirect_if_user_is_authenticated]
+
+    live_session :redirect_if_user_is_authenticated,
+      on_mount: [{FirebnbWeb.UserAuth, :redirect_if_user_is_authenticated}] do
+      live "/users/register", UserRegistrationLive, :new
+      live "/users/log_in", UserLoginLive, :new
+      live "/users/reset_password", UserForgotPasswordLive, :new
+      live "/users/reset_password/:token", UserResetPasswordLive, :edit
+    end
+
+    post "/users/log_in", UserSessionController, :create
+  end
+
+  scope "/", FirebnbWeb do
+    pipe_through [:browser, :require_authenticated_user]
+
+    live_session :require_authenticated_user,
+      on_mount: [{FirebnbWeb.UserAuth, :ensure_authenticated}] do
+      live "/", HomeLive.Index
+      live "/users/settings", UserSettingsLive, :edit
+      live "/users/settings/confirm_email/:token", UserSettingsLive, :confirm_email
+    end
+  end
+
+  scope "/", FirebnbWeb do
+    pipe_through [:browser]
+
+    delete "/users/log_out", UserSessionController, :delete
+
+    live_session :current_user,
+      on_mount: [{FirebnbWeb.UserAuth, :mount_current_user}] do
+      live "/users/confirm/:token", UserConfirmationLive, :edit
+      live "/users/confirm", UserConfirmationInstructionsLive, :new
     end
   end
 end
