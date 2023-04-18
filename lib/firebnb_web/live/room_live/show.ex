dd29a3f8230a120a728a36bddaf44a131c722043
Shankardevy <shankardevy@gmail.com>
2023-04-18 12:00:52 +0100

    Understand two requests from server side

--- lib/firebnb_web/live/room_live/show.ex
+++ lib/firebnb_web/live/room_live/show.ex
@@ -4,6 +4,18 @@ defmodule FirebnbWeb.RoomLive.Show do
 
   @impl true
   def mount(_params, _session, socket) do
+    # Both of these inspect statements will be printed twice
+    # if you visit directly `/rooms/{id}` in the browser because
+    # this `mount/3` is getting called twice -- First as a GET request
+    # and the second as a Websocket request.
+    #
+    # However, if you visit `/room/{id}` by clicking on the links
+    # on the homepage, this `mount/3` function is only called once through
+    # Websocket request as this is a navigation from one liveview to another
+    # with an existing socket connection.
+    IO.inspect "MOUNT IS CALLED", label: "RoomLive mount/3"
+    IO.inspect connected?(socket), label: "Is socket connected?"
+
     {:ok, socket}
   end
 
