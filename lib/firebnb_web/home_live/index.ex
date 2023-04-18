5854e0e479926409e78bdb81056dee1f2104e8aa
Shankardevy <shankardevy@gmail.com>
2023-04-18 01:13:09 +0100

    Load rooms dynamically from db

--- lib/firebnb_web/home_live/index.ex
+++ lib/firebnb_web/home_live/index.ex
@@ -1,4 +1,12 @@
 defmodule FirebnbWeb.HomeLive.Index do
   use FirebnbWeb, :live_view
   import FirebnbWeb.Components
+
+  alias Firebnb.Booking
+
+  def mount(_params, _session, socket) do
+    rooms = Booking.list_rooms()
+
+    {:ok, assign(socket, rooms: rooms)}
+  end
 end
