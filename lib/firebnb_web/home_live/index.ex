910bf701f080a07fd195097ccc1eb8f85b1448fb
Shankardevy <shankardevy@gmail.com>
2023-04-18 01:44:32 +0100

    Handle event in the liveview module

--- lib/firebnb_web/home_live/index.ex
+++ lib/firebnb_web/home_live/index.ex
@@ -7,6 +7,11 @@ defmodule FirebnbWeb.HomeLive.Index do
   def mount(_params, _session, socket) do
     rooms = Booking.list_rooms()
 
-    {:ok, assign(socket, rooms: rooms)}
+    {:ok, assign(socket, rooms: rooms, clicks: 0)}
+  end
+
+  def handle_event("click", _params, socket) do
+    clicks = socket.assigns.clicks + 1
+    {:noreply, assign(socket, clicks: clicks)}
   end
 end
