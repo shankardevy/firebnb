bd990d7de8dbc8e74b22c68adfce0ab751604872
Shankardevy <shankardevy@gmail.com>
2023-04-18 02:20:05 +0100

    Filter rooms by query

--- lib/firebnb/booking.ex
+++ lib/firebnb/booking.ex
@@ -2,8 +2,21 @@ defmodule Firebnb.Booking do
   alias Firebnb.Booking.Room
   alias Firebnb.Repo
 
+  import Ecto.Query
+
   def list_rooms() do
     Room
+    |> Repo.all()
+  end
+
+  def list_rooms(filters) do
+    base_query = Room
+
+    Enum.reduce(filters, base_query, fn
+      {:is_superhost, _}, query -> from room in query, where: room.is_superhost == true
+      {:location, location}, query -> from room in query, where: ilike(room.location, ^location)
+      _, query -> query
+    end)
     |> Repo.all()
   end
 end
