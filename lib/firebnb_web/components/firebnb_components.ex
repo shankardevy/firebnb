0702496927ae644cdc562016e3a89b35caf4282c
Shankardevy <shankardevy@gmail.com>
2023-04-18 02:39:40 +0100

    Filter rooms by superhost

--- lib/firebnb_web/components/firebnb_components.ex
+++ lib/firebnb_web/components/firebnb_components.ex
@@ -68,18 +68,21 @@ defmodule FirebnbWeb.Components do
         type="button"
         class={[
           "relative inline-flex h-6 w-11 flex-shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus:ring-2 focus:ring-amber-500 focus:ring-offset-2",
-          "bg-gray-200"
+          !@superhost && "bg-gray-200",
+          @superhost && "bg-amber-600"
         ]}
         role="switch"
         aria-checked="false"
         aria-labelledby="superhost-label"
+        phx-click="toggle_superhost"
       >
         <!-- Enabled: "translate-x-5", Not Enabled: "translate-x-0" -->
         <span
           aria-hidden="true"
           class={[
             "pointer-events-none inline-block h-5 w-5 transform rounded-full bg-white shadow ring-0 transition duration-200 ease-in-out",
-            "translate-x-0"
+            !@superhost && "translate-x-0",
+            @superhost && "translate-x-5"
           ]}
         >
         </span>
--- lib/firebnb_web/home_live/index.ex
+++ lib/firebnb_web/home_live/index.ex
@@ -7,6 +7,20 @@ defmodule FirebnbWeb.HomeLive.Index do
   def mount(_params, _session, socket) do
     rooms = Booking.list_rooms()
 
-    {:ok, assign(socket, rooms: rooms, clicks: 0)}
+    {:ok, assign(socket, rooms: rooms, superhost: false)}
+  end
+
+  def handle_event("toggle_superhost", _, socket) do
+    superhost = !socket.assigns.superhost
+
+    filters =
+      case superhost do
+        true -> [{:is_superhost, true}]
+        _ -> []
+      end
+
+    rooms = Booking.list_rooms(filters)
+
+    {:noreply, assign(socket, superhost: superhost, rooms: rooms)}
   end
 end
