9f2fdb78c7dbb4cc167158731f618d8aed91523a
Shankardevy <shankardevy@gmail.com>
2023-04-18 09:35:26 +0100

    Add like button to room template

--- lib/firebnb_web/components/firebnb_components.ex
+++ lib/firebnb_web/components/firebnb_components.ex
@@ -32,31 +32,47 @@ defmodule FirebnbWeb.Components do
 
   def room(assigns) do
     ~H"""
-    <a href="" class="relative">
+    <div class="relative">
+      <!-- replace class "text-slate-300 hover:text-red-600" with "text-red-600" to toggle between liked and unliked state -->
+      <button class={[
+        "absolute right-2 top-2",
+        "text-slate-300 hover:text-red-600"
+      ]}>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 20 20"
+          fill="currentColor"
+          class="w-5 h-5"
+        >
+          <path d="M9.653 16.915l-.005-.003-.019-.01a20.759 20.759 0 01-1.162-.682 22.045 22.045 0 01-2.582-1.9C4.045 12.733 2 10.352 2 7.5a4.5 4.5 0 018-2.828A4.5 4.5 0 0118 7.5c0 2.852-2.044 5.233-3.885 6.82a22.049 22.049 0 01-3.744 2.582l-.019.01-.005.003h-.002a.739.739 0 01-.69.001l-.002-.001z" />
+        </svg>
+      </button>
       <div
         :if={@superhost}
         class="absolute left-2 top-2 rounded-sm bg-slate-100 py-0.5 px-1 text-sm font-bold"
       >
         Superhost
       </div>
-      <div>
-        <img
-          :for={image <- @cover_image}
-          src={image.src}
-          class="rounded-lg shadow-lg object-none object-center w-80 h-80"
-        />
-        <h1 class="pt-3 font-bold text-gray-700 line-clamp-2">
-          <%= render_slot(@title) %>
-        </h1>
-      </div>
-
-      <div class="mt-2 text-sm text-gray-600">
-        <%= render_slot(@location) %>
-        <div class="font-bold text-gray-700">
-          €<%= render_slot(@price) %> <span class="font-normal">night</span>
+      <a href="">
+        <div>
+          <img
+            :for={image <- @cover_image}
+            src={image.src}
+            class="rounded-lg shadow-lg object-none object-center w-80 h-80"
+          />
+          <h1 class="pt-3 font-bold text-gray-700 line-clamp-2">
+            <%= render_slot(@title) %>
+          </h1>
         </div>
-      </div>
-    </a>
+
+        <div class="mt-2 text-sm text-gray-600">
+          <%= render_slot(@location) %>
+          <div class="font-bold text-gray-700">
+            €<%= render_slot(@price) %> <span class="font-normal">night</span>
+          </div>
+        </div>
+      </a>
+    </div>
     """
   end
 
