383ee316a24dae268bb71067e29ea9578986a86b
Shankardevy <shankardevy@gmail.com>
2023-04-18 02:27:51 +0100

    Add template for superhost toggle button

--- lib/firebnb_web/components/firebnb_components.ex
+++ lib/firebnb_web/components/firebnb_components.ex
@@ -52,4 +52,37 @@ defmodule FirebnbWeb.Components do
     </a>
     """
   end
+
+  def toggle_superhost(assigns) do
+    ~H"""
+    <div class="flex items-center py-6">
+      <!-- Enabled: "bg-amber-600", Not Enabled: "bg-gray-200" -->
+      <button
+        type="button"
+        class={[
+          "relative inline-flex h-6 w-11 flex-shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus:ring-2 focus:ring-amber-500 focus:ring-offset-2",
+          "bg-gray-200"
+        ]}
+        role="switch"
+        aria-checked="false"
+        aria-labelledby="superhost-label"
+      >
+        <!-- Enabled: "translate-x-5", Not Enabled: "translate-x-0" -->
+        <span
+          aria-hidden="true"
+          class={[
+            "pointer-events-none inline-block h-5 w-5 transform rounded-full bg-white shadow ring-0 transition duration-200 ease-in-out",
+            "translate-x-0"
+          ]}
+        >
+        </span>
+      </button>
+      <span class="ml-3" id="superhost-label">
+        <span class="text-sm font-medium text-gray-900">
+          <%= render_slot(@inner_block) %>
+        </span>
+      </span>
+    </div>
+    """
+  end
 end
